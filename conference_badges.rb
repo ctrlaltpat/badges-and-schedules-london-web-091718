93c53a77f09eaf2795ca5eac8e1de577f36dc181
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 11:49:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(list)
   new_arr = []
   counter = 0
   list.each_with_index do |name, index|
-    new_arr << "Hello, #{name}! You'll be assigned to room #{++counter}"
+    new_arr << "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
   new_arr
 end
