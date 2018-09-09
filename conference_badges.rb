c9037cb6069c2110e279f8ad097b6c9111d6d09c
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 11:43:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr)
-  
+  new_arr = []
+  arr.each do |name|
+    new_arr << badge_maker(name)
+  end
+  new_arr
 end
 
 def assign_rooms(list)
