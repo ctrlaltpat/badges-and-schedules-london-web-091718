cf869d87deed573d18c02cb3a2b8ebfdf60af4b6
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 11:41:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,15 @@
 def badge_maker(name)
-  "Hello my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(arr)
+  
+end
+
+def assign_rooms(list)
+  
+end
+
+def printer
+  
+end
